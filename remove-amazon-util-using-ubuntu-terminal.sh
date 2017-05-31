37349b2f5b356d3a93b71a66c14c6ff634ecc54e
Diwakar Cherukumilli <cherukumilli2@gmail.com>
2017-05-31 13:31:35 -0700

    First draft

---
+++ remove-amazon-util-using-ubuntu-terminal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# remove amazon utils till next update (https://askubuntu.com/questions/760204/how-to-remove-the-unity-amazon-package-in-16-04)
+sudo rm -rf /usr/share/applications/ubuntu-amazon-default.desktop
