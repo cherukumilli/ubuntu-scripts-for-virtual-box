60e5d9c75c8e827df7417fabb5d02c033e0cb6d8
Diwakar Cherukumilli <cherukumilli2@gmail.com>
2017-05-31 13:45:26 -0700

    adds verbose

--- remove-amazon-util-using-ubuntu-terminal.sh
+++ remove-amazon-util-using-ubuntu-terminal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -ev
+
 # remove amazon utils till next update (https://askubuntu.com/questions/760204/how-to-remove-the-unity-amazon-package-in-16-04)
 sudo rm -rf /usr/share/applications/ubuntu-amazon-default.desktop
