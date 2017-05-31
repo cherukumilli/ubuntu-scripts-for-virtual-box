cc95c0be720249a1c1fb959e0ca75a9d0a6724aa
Diwakar Cherukumilli <cherukumilli2@gmail.com>
2017-05-31 13:53:38 -0700

    first draft

---
+++ firefox-uninstall-using-ubuntu-terminal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ev
+
+sudo apt-get --purge autoremove firefox
+sudo rm -rf ~/.mozilla
+sudo rm -rf /etc/firefox
+sudo rm -rf /usr/lib/firefox
+sudo rm -rf /usr/lib/firefox-addons
+sudo reboot
