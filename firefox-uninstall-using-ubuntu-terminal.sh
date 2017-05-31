79e2a7a08458c62150779e66270914fed11b57bf
Diwakar Cherukumilli <cherukumilli2@gmail.com>
2017-05-31 13:57:00 -0700

    execs commands in sequence

--- firefox-uninstall-using-ubuntu-terminal.sh
+++ firefox-uninstall-using-ubuntu-terminal.sh
@@ -2,9 +2,9 @@
 
 set -ev
 
-sudo apt-get --purge autoremove firefox
-sudo rm -rf ~/.mozilla
-sudo rm -rf /etc/firefox
-sudo rm -rf /usr/lib/firefox
-sudo rm -rf /usr/lib/firefox-addons
+sudo apt-get --purge autoremove firefox &&
+sudo rm -rf ~/.mozilla &&
+sudo rm -rf /etc/firefox &&
+sudo rm -rf /usr/lib/firefox &&
+sudo rm -rf /usr/lib/firefox-addons &&
 sudo reboot
